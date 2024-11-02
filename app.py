9603c0d7d2ec890ff273338e548b7f976042ce72
mariusamundsen <marius.amundsen02@gmail.com>
2024-11-02 18:21:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
-from routes.car_routes import car_bp  # Import the car routes
+from routes.car_routes import car_bp        # Import the car routes
+from routes.customer_routes import customer_bp  # Import the customer routes
+from routes.employee_routes import employee_bp  # Import the employee routes
 
 app = Flask(__name__)
-app.register_blueprint(car_bp)  # Register the car blueprint with the app
+
+# Register the blueprints
+app.register_blueprint(car_bp)       # Register the car blueprint
+app.register_blueprint(customer_bp)  # Register the customer blueprint
+app.register_blueprint(employee_bp)  # Register the employee blueprint
 
 @app.route('/')
 def home():
-    return "Here we can see out page!"
+    return "Here we can see out the page."
 
 if __name__ == '__main__':
     app.run(debug=True)
