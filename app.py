703efdaed02afe45e12c59afe9d25fbe3827da8d
teig <egat@live.no>
2024-11-03 13:18:29 +0100

    removing unused apis

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ app = Flask(__name__)
 
 neo4j_driver = get_driver()
 
-app.register_blueprint(car_bp)
-app.register_blueprint(customer_bp)
-app.register_blueprint(employee_bp)
 app.register_blueprint(rental_bp)
 
 
